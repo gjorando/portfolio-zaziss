9eb52725887cf8009c9f48d47be41d6f710c7534
dolfinsbizou <jorandon@gmail.com>
2017-08-13 17:42:32 +0200

    First draft

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$page_title="Zaziss";
+
+require_once("view/index.php");
---
+++ view/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><?= $page_title ?></title>
+	<meta charset="utf8" />
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	<meta name="viewport" content="width=device-width" />
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><img src="/assets/img/logo.jpg" /></li>
+			<li><a href="#about">À propos</a></li>
+			<li><a href="#gallery">Galerie</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+	</nav>
+	<header>
+		<div id="header_content">
+		<h1>Zaziss.com</h1>
+		<blockquote>
+			Si vous pouviez le dire avec des mots, il n'y aurait aucune raison de le peindre.<br /><span class="blockquote_author">Edward Hopper</span>
+		</blockquote>
+		</div>	
+	</header>
+	<section id="about">
+		<h2>Qui suis-je ?</h2>
+		<p>Je suis née à Besançon dans l’est de la France. Créatrice autodidacte, j’étudie actuellement les <strong>arts graphiques</strong> multimédia à Lyon. Je possède également un <abbr title="Certificat de Qualification Professionnelle Animateur Soigneur Assistant">CQP ASA</abbr> dans le milieu de l’équitation. Souhaitant devenir <strong>Game Artist</strong>, j’oriente mes études pour venir à Paris dans les mois à venir...</p>
+		<div class="figure">
+			<img src="assets/img/main_photo.png" alt="me!" width="300px" />
+			<span>Histoire de remplir la page tu peux mettre ici ta photo (les recruteur/euses aiment savoir à qui iels ont affaire), ou un artwork particulier, ou une photo de ton cheval, ou d'un lampadaire, OU D'UN ABRIBUS PUTAIN.</span>
+		</div>
+		<p>Tout les travaux que vous verrez ici sont associés à l’identité <em>Zaziss</em>. Elle existe depuis 2010 pour signer toutes mes créations. Il s’agit également d’un personnage fictif. Enfin pas trop. Pour qu’elle crée, il suffit de lancer son fonctionnement avec un sweatshirt, une gaufre ou un pancake, et une connexion internet !</p>
+	</section>
+	<section id="gallery">
+		<h2>Galerie</h2>
+	</section>
+	<section id="contact">
+		<h2>Contactez moi</h2>
+	</section>
+	<footer>
+		Crafté par : <a href="https://portfolio.intensifi.es/" title="Guillaume's portfolio">Guillaume JORANDON</a>.
+	</footer>
+</body>
+</html>
