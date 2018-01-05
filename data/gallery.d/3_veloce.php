4afead50186b7eb6c347231d0798e73672c4342d
Guillaume Jorandon <guillaume.jorandon@cea.fr>
2018-01-05 16:04:59 +0100

    WIP

---
+++ data/gallery.d/3_veloce.php
@@ -0,0 +1,9 @@
+<?php
+
+$elem = new GalleryElement(
+	'3.png',
+	'Veloce',
+	'Commission',
+	'I tried to make my best for this digital painting. I have improve my skills of lightning with this one. So I like it. She looks so falsely outraged... It is fun. :)',
+	'Common Licence : Please do not use this artwork'
+	);
---
+++ data/gallery.d/4_ribbon_waves.php
@@ -0,0 +1,9 @@
+<?php
+
+$elem = new GalleryElement(
+	'4.png',
+	'Ribbon Waves',
+	'HTC Vive Game, Graphic Design & Game Art',
+	'Ribbon Waves is a VR game project for HTC Vive. Winner of Global Game Jam of Lyon 2017. I have made this example of cover for prints to give at the Japan Touch and Geek Touch convention.',
+	'Common Licence : Please do not use this artwork '
+	);
